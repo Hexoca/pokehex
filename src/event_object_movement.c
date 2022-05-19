bff3f685fcbaf24d4b55d65b2279f99a110bf9ee
Hexoca <80553716+Hexoca@users.noreply.github.com>
2022-05-18 23:13:35 -0700

    Additional Fixes
    Surf Dismount Ground Effects,
    Keep the Camera from Making Waves,
    Implement Missing Text Function RESET_SIZE,
    Not showing dex entries until getting the Pokédex,
    Improving the WaitForVBlank function,
    Better Reflection System

--- src/event_object_movement.c
+++ src/event_object_movement.c
@@ -9138,7 +9138,7 @@ static void DoGroundEffects_OnSpawn(struct ObjectEvent *objEvent, struct Sprite 
 {
     u32 flags;
 
-    if (objEvent->triggerGroundEffectsOnMove)
+    if (objEvent->triggerGroundEffectsOnMove && objEvent->localId != OBJ_EVENT_ID_CAMERA)
     {
         flags = 0;
         UpdateObjectEventElevationAndPriority(objEvent, sprite);
@@ -9154,7 +9154,7 @@ static void DoGroundEffects_OnBeginStep(struct ObjectEvent *objEvent, struct Spr
 {
     u32 flags;
 
-    if (objEvent->triggerGroundEffectsOnMove)
+    if (objEvent->triggerGroundEffectsOnMove && objEvent->localId != OBJ_EVENT_ID_CAMERA)
     {
         flags = 0;
         UpdateObjectEventElevationAndPriority(objEvent, sprite);
@@ -9171,7 +9171,7 @@ static void DoGroundEffects_OnFinishStep(struct ObjectEvent *objEvent, struct Sp
 {
     u32 flags;
 
-    if (objEvent->triggerGroundEffectsOnStop)
+    if (objEvent->triggerGroundEffectsOnStop && objEvent->localId != OBJ_EVENT_ID_CAMERA)
     {
         flags = 0;
         UpdateObjectEventElevationAndPriority(objEvent, sprite);
